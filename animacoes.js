ddc852fc6cb4e83e892f189e8d9525a29a9afb5a
kaua vitorio <kauavitorioof@gmail.com>
2020-10-06 20:56:01 -0300

    CREATE new base for website

--- animacoes.js
+++ animacoes.js
@@ -1,48 +1,7 @@
 window.addEventListener("scroll", function(){
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 150);
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
 })
-
-function limpar()
-{
-    //var nome = document.getElementById(".txtnome")
-    //var email = document.getElementById(".txtemail")
-
-    document.getElementById("inputnome").value = "";
-    document.getElementById("inputEmail").value = "";
-    document.getElementById("inputtext").value = "";
-    document.getElementById("inputnome").focus();
+function abreMenu(){
+    
 }
-
-function enviar()
-{
-    var nome =  document.getElementById("inputnome");
-    var email = document.getElementById("inputEmail");
-    var textt =  document.getElementById("inputtext");
-
-    if (nome.value == "") {
-        alert("Nome não informado!");
-        nome.focus();
-        return;
-    }
-    if (email.value == "") {
-        alert("Email não informado!");
-        email.focus();
-        return;
-    }
-    if (textt.value == "") {
-        alert("Mensagem não informado!");
-        textt.focus();
-        return;
-    }
-
-    alert("Sua duvida foi enviada para Kauã, em breve você recebera uma resposta em seu email")
-    nome.value = "";
-    email.value = "";
-    textt.value = "";
-}
-
-function aviso(){
-    alert("❗️ O recurso de duvida esta indisponivel no momento ❗️\n🔨 Estou corigindo alguns bugs 🔨 ");
-}
-
