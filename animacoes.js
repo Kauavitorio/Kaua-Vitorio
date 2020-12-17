315a0a2de399e34f89cdfdc515cf781c26b22233
kaua vitorio <kauavitorioof@gmail.com>
2020-12-17 15:24:32 -0300

    fix responsive and new host

--- animacoes.js
+++ animacoes.js
@@ -3,5 +3,11 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 function abreMenu(){
-    
+    document.getElementById("menu").style.height = "100vh";
+    document.getElementById("menu").style.backgroundColor = "black";
+    document.getElementById("menu2").style.left = "-80%";
+    document.getElementById("menu2").style.height = "800%";
+    document.getElementById("menu2").style.display = "block";
+    document.getElementById("menu2").style.visibility = "visible";
+    document.getElementById("close").style.visibility = "visible";
 }
--- index-login.php
+++ index-login.php
@@ -5,19 +5,19 @@
 
 	if($login && $senha !='') {
 		//estabelecendo a conexão com banco de dados
-		$conexao=mysql_pconnect('localhost','root','') or die("Problema ao efetuar a conexão com banco de dados");	
+		$conexao=mysql_pconnect('sql211.epizy.com','epiz_27486279','HkOzAtaRdQSY') or die("Problema ao efetuar a conexão com banco de dados");	
 		//abrindo o banco de dados que foi criado na área phpMyAdmin
-		$conecta_banco=mysql_select_db('kauavitoriosite',$conexao) or die("Problema ao abrir o banco de dados");
+		$conecta_banco=mysql_select_db('epiz_27486279_kauavitoriosite',$conexao) or die("Problema ao abrir o banco de dados");
 		//executando o comando sql para listar os registros da tabela
 		$comando=mysql_query("select * from tb_login where usuario_user='$login' and 	senha_user = '$senha'");
 		
 		if(mysql_num_rows($comando) > 0) {	// se resultou algum registro(linha)
 			echo "<script language=javascript> window.alert('Login realizado com sucesso, bem vindo.');</script>";	
-            include 'index-principal.html';	
+            include 'index.html';	
 		}
 		else {
 			echo "<script language=javascript> window.alert('Desculpe, não encontramos uma conta com esse nome de Usuario. Tente novamente ou crie um nova conta.');</script>";
-            include 'index.html';
+            include 'index-login.html';
 		}
 		//fechando o banco de dados
 		$fecha_banco=mysql_close($conexao);
@@ -25,3 +25,5 @@
 	else {
         echo"<script language='JavaScript'>window.alert('Preencha todos os campos.');history.back();</script>";
 	}
+
+	mysqli_set_charset($conexao, "utf8");
