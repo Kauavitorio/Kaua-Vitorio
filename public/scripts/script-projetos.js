8877bffe3b200767d1a84678d8f38e4cc0ef6eb5
kauavitorio <kauavitorioof@gmail.com>
2021-01-29 22:01:19 -0600

    set cookie

--- public/scripts/script-projetos.js
+++ public/scripts/script-projetos.js
@@ -1,4 +1,47 @@
+ // ------------  Start Cookie create --------------------
+ function createCookie(lang, value, expira){
+    var dtExpira = "expires="+ expira;
+    document.cookie = lang + "=" + value + "; " + dtExpira;     
+}
 
+function ReadCookie(lang){
+    var linguagem = lang + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++){
+        var c = ca[i];
+        while (c.charAt(0)==' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(linguagem) == 0)
+        return c.substring(linguagem.length,c.length);
+    }
+    return "";
+}
+
+function initCookie(){
+    var linguagemDef = ReadCookie("linguagem")
+    if(linguagemDef == "English" || linguagemDef == "EN-US"){
+        change_to_en();
+            btn_mudar_idioma_ingles.style.display = "none";
+    }else if(linguagemDef == "Portugues" || linguagemDef == "Português" || linguagemDef == "PT-BR"){
+        change_to_ptbr();
+    }
+    else{
+        // linguagemDef = prompt('Escolha um idioma\nChoose a language\nPT-BR = "Português",\nEN-US = "English"', "" );
+        linguagemDef = prompt('Escolha um idioma || Choose a language\nPT-BR = "Português" || EN-US = "English"', "" );
+        if (linguagemDef == "" && linguagemDef == null){
+            window.location.reload();
+        }else if (linguagemDef == "English" || linguagemDef == "EN-US"){
+            createCookie("linguagem", linguagemDef, " Tue, 01 Jan 2060 12:00:00 UTC");
+            window.location.reload();
+        }else if (linguagemDef == "Portugues" || linguagemDef == "Português" || linguagemDef == "PT-BR"){
+            createCookie("linguagem", linguagemDef, " Tue, 01 Jan 2060 12:00:00 UTC");
+            window.location.reload();
+        }else {
+            window.location.reload();
+        }
+    }
+}// -------------------- End Cookie Create   --------------------
 
 function change_to_en(){
 }
