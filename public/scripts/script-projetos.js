9929f8542a751919aa8f57a94f84ff20d336aff9
kauavitorio <kauavitorioof@gmail.com>
2021-01-29 21:55:26 -0600

    create portugues translate

---
+++ public/scripts/script-projetos.js
@@ -0,0 +1,14 @@
+
+
+function change_to_en(){
+}
+
+function change_to_ptbr(){
+    document.getElementById("txt_recent_projcts").innerHTML = "PROJETOS RECENTES"
+    document.getElementById("txt_about_palacepetz").innerHTML = "Esse projeto foi desenvolvido para meu trabalho de conclusão de modulo, desenvolvido no visual studio, com a linguagem csharp, com o intuito de criar um sistema de gerenciamento para uma empresa de pet shop"
+    document.getElementById("txt_view_project_palace").innerHTML = "Visualizar Projeto ->"
+    document.getElementById("txt_desc_system_mobile").innerHTML = "Esse projeto foi desenvolvido para meu trabalho de conclusão do 2° modulo, desenvolvido no Android Studio, com a linguagem java, com o intuito de criar uma aplicação de uso interno dos sócios da System Strength servindo como agenda aos sócios."
+    document.getElementById("txt_view_project_system_mobile").innerHTML = "Visualizar Projeto ->"
+    document.getElementById("txt_desc_coffee_project").innerHTML = "Esse projeto está em desenvolvimento, ele está sendo desenvolvido no Android Studio com a linguagem Java. Seu intuito é minha aprendizagem e de meus companheiros de estudo."
+    document.getElementById("txt_knowmore_project_coffee").innerHTML = "Saiba Mais..."
+}
