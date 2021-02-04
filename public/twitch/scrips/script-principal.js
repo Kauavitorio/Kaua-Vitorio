0ded677962c7f76ea4d500fb5fd8fcd6ced71f25
kauavitorio <kauavitorioof@gmail.com>
2021-02-03 20:31:08 -0600

    replace twitch

--- public/twitch/scrips/script-principal.js
+++ public/twitch/scrips/script-principal.js
@@ -1,3 +1,19 @@
-function GoPartner(){
-    window.location.href = "http://www.devmedia.com.br";
-}
+// Option 2 - jQuery Smooth Scrolling
+// $('.navbar a').on('click', function (e) {
+//   if (this.hash !== '') {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body')
+//       .animate({
+//         scrollTop: $(hash).offset().top
+//       },800);
+//   }
+// });
+
+
+ // Option 3 - Smooth Scroll - https://github.com/cferdinandi/smooth-scroll
+//  const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+// 	speed: 500
+// });
