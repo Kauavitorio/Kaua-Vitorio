688e5c2fa49d756ac31e44d856e56b61d4165b13
kaua vitorio <kauavitorioof@gmail.com>
2021-02-21 12:44:52 -0300

    set texto to download

--- public/projects/scripts/find-address.js
+++ public/projects/scripts/find-address.js
@@ -51,7 +51,9 @@ function change_to_en(){
     document.getElementById("txt_intro").innerHTML = "Introduction"
     document.getElementById("txt_projects").innerHTML = "Projects"
     document.getElementById("text_desciption_project").innerHTML = "This is a project that I am developing for my learning and training what I am learning my course, this project is in version 1.0, start date 02/17/21."
-    document.getElementById("btn_download").value = "Download here"}
+    document.getElementById("btn_download").value = "Download here"
+    document.getElementById("txt_desc_download").innerHTML = "If you have interest for my project, you can download app right now just clicking this button.Have a fun<br/>App size: 14 mb<br/>Need network to use app? Yes"
+}
 
 function change_to_ptbr(){
     btn_mudar_idioma_ptbr.style.display = "none",
@@ -60,4 +62,6 @@ function change_to_ptbr(){
     document.getElementById("txt_intro").innerHTML = "Introdução"
     document.getElementById("txt_projects").innerHTML = "Projetos"
     document.getElementById("text_desciption_project").innerHTML = "Esse é um projeto que estou desenvolvendo para meu aprendizado e treinar oque estou apreendendo meu meu curso, esse projeto esta na versão 1.0, data de inicio 17/02/21."
-    document.getElementById("btn_download").value = "Baixar aqui"}
+    document.getElementById("btn_download").value = "Baixar aqui"
+    document.getElementById("txt_desc_download").innerHTML = "Se você tem interesse no meu projeto, pode baixar o aplicativo agora mesmo clicando neste botão. Divirta-se<br/>Tamanho do aplicativo: 14 MB<br/>Precisa de rede para usar o aplicativo? Sim"
+}
