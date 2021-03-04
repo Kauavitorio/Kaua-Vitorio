cffb005697e336edf28b17099415b3bd6605650b
kaua vitorio <kauavitorioof@gmail.com>
2021-03-04 11:17:45 -0300

    create loading animation

---
+++ public/LoadingAnimation/script.js
@@ -0,0 +1,6 @@
+let spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+    window.addEventListener('load', function () {
+        // spinnerWrapper.style.display = 'none';
+        spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+    });
